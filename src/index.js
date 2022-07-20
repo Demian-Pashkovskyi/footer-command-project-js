698196b899e72ed2ff872531e7dfe01b553a5ccd
Demian Pashkovskyi <skyangely4@gmail.com>
2022-07-20 12:58:46 +0300

    as

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import '../src/js/pagination-result';
 import '../src/js/to-top-btn';
+import '../src/js/api-fetch';
+import '../src/js/spiner';
+import '../src/js/localstorage';
+import '../src/js/modal';
